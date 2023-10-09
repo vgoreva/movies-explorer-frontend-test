d7586cf621a27f69f6d01725aa6c6578d67ae17c
Goreva Vlada <119337508+vgoreva@users.noreply.github.com>
2023-10-10 01:26:33 +0300

    Delete src directory

--- src/components/Promo/Promo.jsx
+++
@@ -1,9 +0,0 @@
-function Promo() {
-    return (
-        <section className="promo">
-            <h1 className="promo__header">Учебный проект студента факультета Веб-разработки.</h1>
-        </section>
-    )
-}
-
-export default Promo
